7fb3f3f234993bc74fe5500cc6612f76bc6df64e
RomanAtreides <justaspeckinspace@gmail.com>
2021-11-19 16:56:20 +0300

    Change files Praktikum.java and Praktikum.class

--- Praktikum.java
+++ Praktikum.java
@@ -27,7 +27,6 @@ public class Praktikum {
             } else if (command == 2) {
                 dinnerAdvisor.getAdvice(moneyBeforeSalary, daysBeforeSalary);
             } else if (command == 3) {
-                // Печать вопросов и считывание ответов оставьте в классе Praktikum
                 System.out.println("За какой день вы хотите ввести трату: 1-ПН, 2-ВТ, 3-СР, 4-ЧТ, 5-ПТ, 6-СБ, 7-ВС?");
                 int day = scanner.nextInt();
                 System.out.println("Введите размер траты:");
@@ -41,14 +40,14 @@ public class Praktikum {
                 System.out.println("Выход");
                 break;
             } else {
-                System.out.println("Извините, такой команды пока нет.");
+                System.out.println("Извините, такой команды нет.");
             }
         }
         scanner.close();
     }
 
     public static void printMenu() {
-        System.out.println("Что вы хотите сделать? ");
+        System.out.println("Что вы хотите сделать?");
         System.out.println("1 - Конвертировать валюту");
         System.out.println("2 - Получить совет");
         System.out.println("3 - Ввести трату");
